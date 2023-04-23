73b8d22f9e46cd5c4d73e4a9319f365132a534da
Gino Luraschi <gluraschi@iqhive.com>
2023-04-24 10:12:54 +1200

    fix: Fix mockk test

--- src/test/kotlin/com/codely/testingexample/AppTestWithMockk.kt
+++ src/test/kotlin/com/codely/testingexample/AppTestWithMockk.kt
@@ -10,7 +10,7 @@ class AppTestWithMockk {
     @Test
     fun `should calculate the difference and return 31 years`() {
         val reader = mockk<Reader>()
-        val writer = mockk<Writer>(relaxed = true)
+        val writer = mockk<Writer>(relaxed = true)// Omit lines to mock
         val clock = mockk<Clock>()
         val app = App(writer,reader, clock)
         every { reader.read() } returns "1990-08-31"
@@ -18,12 +18,6 @@ class AppTestWithMockk {
 
         app.execute()
 
-        verify { writer.write("The difference between the date you wrote an today is 31 years") }
-        /*
-        1) Writer(#2).write(Please enter a date with the format <yyyy-MM-dd>)
-        2) Writer(#2).write(Bye!)
-        3) Writer(#2).write(You wrote the date 1990-08-31)
-        4) Writer(#2).write(The age is 31 years)
-         */
+        verify { writer.write("The age is 31 years") }
     }
 }
